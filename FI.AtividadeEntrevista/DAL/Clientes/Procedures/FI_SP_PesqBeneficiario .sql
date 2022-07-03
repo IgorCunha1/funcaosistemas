d061b787140bf5c88284235529caa75ae43ca248
igor Silva <igorivson@outlook.com>
2022-07-03 01:02:09 -0300

    Novo

---
+++ FI.AtividadeEntrevista/DAL/Clientes/Procedures/FI_SP_PesqBeneficiario .sql
@@ -0,0 +1,35 @@
+﻿CREATE PROC FI_SP_PesqBeneficiario
+	@iniciarEm int,
+	@quantidade int,
+	@campoOrdenacao varchar(200),
+	@crescente bit,
+	@idCliente BIGINT
+AS
+BEGIN
+	DECLARE @SCRIPT NVARCHAR(MAX)
+	DECLARE @CAMPOS NVARCHAR(MAX)
+	DECLARE @ORDER VARCHAR(50)
+	
+	IF(@campoOrdenacao = 'ID')
+		SET @ORDER =  ' ID '
+	ELSE
+		SET @ORDER = ' NOME '
+
+	IF(@crescente = 0)
+		SET @ORDER = @ORDER + ' DESC'
+	ELSE
+		SET @ORDER = @ORDER + ' ASC'
+
+	SET @CAMPOS = '@iniciarEm int,@quantidade int'
+	SET @SCRIPT = 
+	'SELECT ID, NOME, CPF, IDCLIENTE FROM
+		(SELECT ROW_NUMBER() OVER (ORDER BY ' + @ORDER + ') AS Row, IID, NOME, CPF, IDCLIENTE FROM BENEFICIARIOS WITH(NOLOCK))
+		AS ClientesWithRowNumbers
+	WHERE IDCLIENTE = @idCliente and Row > @iniciarEm AND Row <= (@iniciarEm+@quantidade) ORDER BY'
+	
+	SET @SCRIPT = @SCRIPT + @ORDER
+			
+	EXECUTE SP_EXECUTESQL @SCRIPT, @CAMPOS, @iniciarEm, @quantidade
+
+	SELECT COUNT(1) FROM CLIENTES WITH(NOLOCK)
+END
---
+++ FI.WebAtividadeEntrevista/Scripts/site.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#cpf').mask('000.000.000-00');
+
+});
+
+ $('#cpf').blur(function(){
+     var cpf = $('#cpf').val();
+     var msg_error = $('#msg_error');
+    
+     if(cpf.length < 14){
+        msg_error.html('<div class="alert alert-danger" role="alert">Cpf precisa ter 11 digitos</div>');
+    }else{
+        msg_error.html('')
+     }
+    
+ })
+
